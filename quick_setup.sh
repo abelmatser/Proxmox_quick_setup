7dd4fbefe49145d5ace59b23d2f2a551f5875581
Abel Matser <33466359+abelmatser@users.noreply.github.com>
2019-11-10 14:23:28 +0100

    Copy quick_setup from private repository

---
+++ quick_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo 'Changing HandleLidSwitch'
+sed 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
+
+echo 'Configuring Wake on Lan'
+apt install ethtool
+
+echo "
+# Reset WOL daily and after reboot
+30 22 * * * root /sbin/ethtool -s enp3s0 wol g
+@reboot root /sbin/ethtool -s enp3s0 wol g
+" >> /etc/crontab
+
+echo 'Configuring daily shutdown'
+echo "
+# shutdown every night
+30 23 * * * root shutdown -h now
+" >> /etc/crontab
+
+echo 'Changing Proxmox subscription (only valid for stretch)'
+cd /etc/apt/sources.list.d/
+
+sed 's/deb/#deb/' pve-enterprise.list
+
+echo 'deb http://download.proxmox.com/debian/pve stretch pve-no-subscription' > pve-no-subscription.list
